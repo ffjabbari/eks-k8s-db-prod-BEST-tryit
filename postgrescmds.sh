c6fda6a4f413049e261e603c85628e4ec4dee13f
Jorge Clavijo <jym272@gmail.com>
2023-01-20 02:52:15 -0300

    feat: liveness and readiness for db

--- postgrescmds.sh
+++ postgrescmds.sh
@@ -10,3 +10,5 @@ kubectl delete svc db-service
 users_api=$(minikube service load-balancer-multinetwork --url | head -n1)
 
 tasks_api=$(minikube service load-balancer-multinetwork --url | tail -n1)
+
+kubectl logs -f $pod init-postgres
