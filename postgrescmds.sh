436ae1834da3b25f54e36258cf9ffef35293323a
Jorge Clavijo <jym272@gmail.com>
2023-01-19 21:49:20 -0300

    feat: kubernetes with dns pods

--- postgrescmds.sh
+++ postgrescmds.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 psql -U postgres
+
+kubectl get svc
+
+#delete svc db-service
+kubectl delete svc db-service
+
+users_api=$(minikube service load-balancer-multinetwork --url | head -n1)
+
+tasks_api=$(minikube service load-balancer-multinetwork --url | tail -n1)
