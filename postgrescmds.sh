312aa5fd83fcdc31aa65fa425d83335565e716fb
Jorge Clavijo <jym272@gmail.com>
2023-02-15 22:34:13 -0300

    kubectl utility container added

--- postgrescmds.sh
+++ postgrescmds.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-psql -U postgres
+aws cloudformation create-stack \
+  --region sa-east-1 \
+  --stack-name my-eks-vpc-stack-demo \
+  --template-url https://s3.us-west-2.amazonaws.com/amazon-eks/cloudformation/2020-10-29/amazon-eks-vpc-private-subnets.yaml
 
-kubectl get svc
 
-#delete svc db-service
-kubectl delete svc db-service
+export AWS_REGION=sa-east-1
 
-users_api=$(minikube service load-balancer-multinetwork --url | head -n1)
+# list all cluster in eks output jq
+aws eks list-clusters
+# generate kubeconfig and save it to ~/.kube/config -> THIS REPLACE THE CURRENT CONFIG
+aws eks update-kubeconfig --region sa-east-1 --name test-demo
+# ! now kubectl is configured to use the cluster
 
-tasks_api=$(minikube service load-balancer-multinetwork --url | tail -n1)
+aws eks --region sa-east-1 update-kubeconfig --name test-demo --role-arn arn:aws:iam::712136406596:role/myAmazonEKSClusterRoleDemo
 
-kubectl logs -f $pod init-postgres
